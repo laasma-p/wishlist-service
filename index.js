a6c5d0659bf19387a238ce2f250c57189b2497f9
Lasma Poksane <laasmapoksane@gmail.com>
2025-09-03 20:45:44 +0200

    Render routes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const wishlistRoutes = require("./routes/wishlist-routes");
+const itemRoutes = require("./routes/item-routes");
+const purchaseRoutes = require("./routes/purchase-routes");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/wishlist", wishlistRoutes);
+app.use("/item", itemRoutes);
+app.use("/purchase", purchaseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
