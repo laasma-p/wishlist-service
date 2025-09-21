9ae6e7d793ed601203c9ea4a002b769c964f76f0
Lasma Poksane <laasmapoksane@gmail.com>
2025-09-22 00:01:17 +0200

    Render example wishlists page

--- routes/wishlist-routes.js
+++ routes/wishlist-routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // getting all wishlists
+  res.render("wishlists");
 });
 
 router.post("/", (req, res, next) => {
